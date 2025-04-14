833b1917d2a11e6f0aa7490e0af5ecc82b225302
김현정 <ajdrns8694@gmail.com>
2025-04-14 16:21:01 +0900

    :sparkles: 연습문제 7-27 내부클래스와 외부클래스에 선언된 변수의 이름이 같을 경우

---
+++ src/Ex22.java
@@ -0,0 +1,23 @@
+class Outer2 {
+    int value = 10;
+
+    class Inner {
+        int value = 20;
+        void method1(){
+            int value = 30;
+
+            System.out.println("value : " + value);
+            System.out.println("this.value : " + this.value);
+            System.out.println("Outer2.this.value : " + Outer2.this.value);
+        }
+    }
+}
+
+public class Ex22 {
+    public static void main(String[] args) {
+        Outer2 outer2 = new Outer2();
+        Outer2.Inner oi = outer2.new Inner();
+
+        oi.method1();
+    }
+}
